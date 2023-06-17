52d2c09d80d2b014f7f96b70cc23056a357b9c9b
Gabriel Ribeiro Domingues <gabrielrd123@gmail.com>
2023-06-17 00:25:15 -0300

    classes usuario e multa

---
+++ PJBL9-Biblioteca/Biblioteca/Biblioteca/Multa.java
@@ -0,0 +1,14 @@
+package Biblioteca;
+
+public class Multa extends Exception {
+    private float valorMulta;
+
+    public Multa(String message, float valorMulta) {
+        super(message);
+        this.valorMulta = valorMulta;
+    }
+
+    public float getValorMulta() {
+        return valorMulta;
+    }
+}
---
+++ PJBL9-Biblioteca/Biblioteca/Biblioteca/Usuario.java
@@ -0,0 +1,12 @@
+package Biblioteca;
+
+public class Usuario {
+    int IdUsuario;
+    String NomeAluno;
+
+    public Usuario(int IdUsuario, String NomeAluno) {
+        this.IdUsuario = IdUsuario;
+        this.NomeAluno = NomeAluno;
+    }
+    
+}
