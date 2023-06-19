cc13bd8598696525415750ad9f6d301e52203760
diogonegrelli <diogonegrelli@outlook.com>
2023-06-18 23:28:32 -0300

    desenvolvimento classes usuario, livro e main

--- PJBL9-Biblioteca/Biblioteca/Biblioteca/Main.java
+++ PJBL9-Biblioteca/Biblioteca/Biblioteca/Main.java
@@ -15,10 +15,25 @@ public class Main {
         Categoria eletronicos = new Categoria(012023, "Eletrônicos", colecaoEletronicos);
         Categoria livraria = new Categoria(022023, "Livraria" , colecaoLivraria);
         //*CATEGORIAS CRIADAS//
-        itemBiblioteca dell = new Notebook(545, "DELL INSPIRON 2023" , "corredor 04", true, 02, "dell" , "inspiron 2023", eletronicos);
+
+
+
+        //* CRIAÇAO DOS ITENS DA BIBLIOTECA//
+        itemBiblioteca notebook1 = new Notebook (544, "ACER ASPIRE 5", "corredor 04", true, 03, "ACER" , "aspire 5" , eletronicos);
+        itemBiblioteca notebook2 = new Notebook(545, "DELL INSPIRON 2023" , "corredor 04", true, 02, "DELL" , "inspiron 2023", eletronicos);
+        itemBiblioteca revista1 = new Revista(546,"")
+        itemBiblioteca revista2 = new Revista(547,)
+        itemBiblioteca revista3 = new Revista(548,)
+        itemBiblioteca livro1 = new Livro(549,);
+        itemBiblioteca livro2 = new Livro(550,);
+        itemBiblioteca livro3 = new Livro(551,);
+
+        colecaoEletronicos.add(notebook1);
+        colecaoEletronicos
 
         Revista revista = new Revista(1, "Título da Revista", "Localização da Revista", true, 10, "Periodico da Revista", 1, "Editora da Revista", livraria);
         System.out.println(revista.calculaMulta(5));
+
 
 
     }
--- PJBL9-Biblioteca/Biblioteca/Biblioteca/Usuario.java
+++ PJBL9-Biblioteca/Biblioteca/Biblioteca/Usuario.java
@@ -1,12 +1,19 @@
 package Biblioteca;
 
-public class Usuario {
-    int IdUsuario;
-    String NomeAluno;
+import java.util.ArrayList;
 
-    public Usuario(int IdUsuario, String NomeAluno) {
+public class Usuario {
+    private int IdUsuario;
+    private String NomeAluno;
+    private ArrayList<Emprestimo> emprestimosUsuario;
+
+
+
+    public Usuario(int IdUsuario, String NomeAluno, ArrayList<Emprestimo>emprestimosUsuario) {
         this.IdUsuario = IdUsuario;
         this.NomeAluno = NomeAluno;
+        this.emprestimosUsuario = emprestimosUsuario;
+
     }
 
     public int getIdUsuario() {
